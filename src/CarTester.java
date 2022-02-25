bce01d37fe63f0420f1509c90906d9cd4d1f64d0
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-02-25 12:36:30 -0500

    Add files via upload

---
+++ src/CarTester.java
@@ -0,0 +1,21 @@
+public class CarTester
+{
+    public static void main(String[] args)
+    {
+        // Create a Car object
+        
+        // Print out the model
+        
+        // Print out the MPG
+        
+        // Print the object
+        
+        // Create an ElectricCar object
+        
+        // Print out the model
+        
+        // Print out the MPG
+        
+        // Print the object
+    }
+}
---
+++ src/ElectricCar.java
@@ -0,0 +1,16 @@
+public class ElectricCar extends Car {
+
+    // Complete the constructor
+    public ElectricCar(String model){
+       
+    }
+
+    // Override the getMPG here.
+    // It should return: "Electric cars do not calculate MPG.
+    
+
+    // Override the toString() here.
+    // (model) is an  electric car.
+    
+    
+}
