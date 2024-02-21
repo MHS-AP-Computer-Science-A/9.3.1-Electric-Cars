662ebeef45af200c6de56b8fb24350845ad9974e
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-02-21 10:02:46 -0500

    Update CarTester.java

--- src/CarTester.java
+++ src/CarTester.java
@@ -2,20 +2,19 @@ public class CarTester
 {
     public static void main(String[] args)
     {
-        // Create a Car object
-        
+        // Create a Car object that represents a "Camero" with MPG of 30
+  
         // Print out the model
-        
+       
+        // Print out the MPG
+       
+   
+
+        // Create an ElectricCar object that represents a "Telsa" with a range of 200 miles
+       
+        // Print out the model
+       
         // Print out the MPG
         
-        // Print the object
-        
-        // Create an ElectricCar object
-        
-        // Print out the model
-        
-        // Print out the MPG
-        
-        // Print the object
     }
 }
